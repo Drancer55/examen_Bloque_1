c16eba1ede7129f5f51f3757b7e8e7cc620166bf
Drancer55 <antonio281255@gmail.com>
2021-11-11 12:21:10 -0600

    avance del examen

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 fetch("./data.json")
 .then((response) => response.json())
 .then((bootcampData) => console.log(bootcampData))
-.catch((error)=> console.log(error))
+.catch((error) => console.log(error))
 
-const miembros = document.getElementById('BootcampMembers')
-
-const iterando = (bootcampData) => {
+let iterar = (bootcampData) => {
     console.log(bootcampData)
     for (const item of bootcampData.BootcampCrew) {
         console.log(item);
     }
 }
-    //miembros.innerHTML += `<div> `${item.Nombre}` </div>`
-    
+const miembros = document.getElementById('BootcampMembers')
+//miembros.innerHTML = `${item.Nombre}` + " " + `${item.Apellido}`
