742061360ff284144c6645fa131191c6d1e11e86
Drancer55 <antonio281255@gmail.com>
2021-11-15 01:03:26 -0600

    revisión de detalles

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 fetch("./data.json")
 .then((response) => response.json())
-.then((bootcampData) => console.log(bootcampData))
+.then((data) => bootcampData(data))
 .catch((error) => console.log(error))
 
-let iterar = (bootcampData) => {
-    console.log(bootcampData)
-    for (const item of bootcampData.BootcampCrew) {
-        console.log(item);
+let bootcampData = (data) => {
+    console.log(data)
+    for (const item of data.BootcampCrew) {
+        console.log(data.BootcampCrew);
+        console.log(item.Nombre);
+        const miembros = document.getElementById('BootcampMembers')
+        miembros.innerHTML += `<b> Nombre: </b> ${item.Nombre}` + " " + `${item.Apellido}` + `<br>` +
+        `<td>` + `<b> Edad: </b> ${item.Edad}` + `<br>` +
+        `<td>` + `<b>Cumpleños: </b> ${item.Cumpleaños}` + `<br>`+ `<hr>`;
     }
 }
-const miembros = document.getElementById('BootcampMembers')
-//miembros.innerHTML = `${item.Nombre}` + " " + `${item.Apellido}`
